84cc8f511f7485ec1b96dd7003d99351a2a0e07d
Daniloemejias <daniloemejias@gmail.com>
2023-04-04 02:53:04 +0000

    trafic-light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor]=useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container fluid d-flex justify-content-center">
+
+			<div className="trafic-light d-flex flex-column py-3 align-items-center rounded">
+
+				<div onClick={()=>setColor("red")} className= {`${color=="red"?"glow":""} red rounded-circle`}></div>
+
+				<div onClick={()=>setColor("yellow")} className= {`${color=="yellow"?"glow":""} yellow rounded-circle my-3`}></div>
+
+				<div onClick={()=>setColor("green")} className= {`${color=="green"?"glow":""} green rounded-circle`}></div>
+
+			</div>
+			
 		</div>
+		
+
 	);
 };
 
